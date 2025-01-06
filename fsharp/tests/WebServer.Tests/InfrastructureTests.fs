acf6fc98ca592b3dda9cf2e6c0853e1d22d14173
Connor Keenum <nellakprime@gmail.com>
2025-01-05 20:31:35 -0600

    Sucessfully setup and seed test database using proper async primitives!

--- fsharp/tests/WebServer.Tests/InfrastructureTests.fs
+++ fsharp/tests/WebServer.Tests/InfrastructureTests.fs
@@ -1,6 +1,6 @@
 // Tests for infrastructure code
 namespace Tests.InfrastructureTests
-open FsCheck
+
 open Xunit
 open Tests.TestDatabaseHelpers.EnvOps
 open Tests.TestDatabaseHelpers.DBOps
@@ -9,23 +9,30 @@ open Tests.TestDatabaseHelpers.Teardown
 open SE2.Infrastructure.Persistence.SQLc
 open Npgsql
 open System
+open FsToolkit.ErrorHandling
 
 // TODO: Make all namespaces standardized
-module GetAllUsers =    
-    let DB_CONNECTION = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
+module GetAllUsers =
+    let DEFAULT_DB_CONNECTION = getEnv "DEFAULT_DB_CONNECTION_STRING"
+    let DB_CONNECTION = getEnv "DB_CONNECTION_STRING"
+    let DB_NAME = getEnv "TEST_DATABASE_NAME"
+    let SCHEMA_PATH = schemaPath ()
     let db = DB(DB_CONNECTION)
 
     [<Fact>]
     let ```Test getAllUsers should retrieve all users`` () =
-        printfn "Connection String: '%s'" DB_CONNECTION
-        printfn "\n"
-        printfn "Current working directory: %s" (Environment.CurrentDirectory)
-        printfn "Going up: %s" (schemaPath())
-        printfn "\n"
-        printfn "%s"(getFileData (schemaPath()))
-        //createTestDB ()
-        // seedTestDB "envPath" "DB_ENV" "seedPath" "SEED_SUCCESS"
-        // try
-        //     Assert.NotEmpty(db.getAllUsers())
-        // finally
-        //     dropTestDatabase "envPath" "POSTGRES_ENV" "DB_NAME_ENV" "DROP_SUCCESS"
+        let test =
+            createTestDB ()
+            |> Async.RunSynchronously
+            |> (fun value ->
+                match value with
+                | Ok() -> "Database setup successfully"
+                | Error err -> err)
+
+        printfn "%s" test
+
+// seedTestDB "envPath" "DB_ENV" "seedPath" "SEED_SUCCESS"
+// try
+//     Assert.NotEmpty(db.getAllUsers())
+// finally
+//     dropTestDatabase "envPath" "POSTGRES_ENV" "DB_NAME_ENV" "DROP_SUCCESS"
--- fsharp/tests/WebServer.Tests/Utilities/TestDatabaseHelpers.fs
+++ fsharp/tests/WebServer.Tests/Utilities/TestDatabaseHelpers.fs
@@ -5,7 +5,11 @@ open System
 open System.IO
 open System.Diagnostics
 open Npgsql
-open dotenv.net
+open FsToolkit.ErrorHandling
+
+// TODO: DbError type instead of non-semantic strings
+// TODO: getEnv to option instead
+// TODO: use @ and ..\ to make the paths platform independent like @"..\..\..path"
 
 // EnvOps - wrappers for environment variable functions like getting an env or a relative path
 module EnvOps =
@@ -18,106 +22,109 @@ module EnvOps =
     let schemaPath () = // convienience function for schema file location
         pathFromRelative "../../../../../src/Infrastructure/WebServer.Infrastructure/Persistence/schema.sql"
 
+    let seedPath () = // convienience function for seed file location
+        pathFromRelative "../../../../../src/Infrastructure/WebServer.Infrastructure/Persistence/seeds/seed.sql"
+
 // DBOps - wrappers for basic DB operations like getting data, getting env, executing SQL, and more
 module DBOps =
+    let querySQL (db: NpgsqlConnection) (sql: string) =
+        async {
+            try
+                use command = new NpgsqlCommand(sql, db)
+                use reader = command.ExecuteReader()
+
+                let results =
+                    seq {
+                        while reader.Read() do
+                            yield [ for i in 0 .. reader.FieldCount - 1 -> reader.GetValue(i) ]
+                    }
+                    |> Seq.toList
+
+                return Ok results
+            with ex ->
+                return Error $"Error querying SQL: \n{ex.Message}"
+        }
+
     let executeSQL (db: NpgsqlConnection) (sql: string) =
-        try
-            use command = new NpgsqlCommand(sql, db)
-            command.ExecuteNonQuery() |> ignore
-        with ex ->
-            printfn "Error executing SQL: %s" ex.Message
+        async {
+            try
+                use command = new NpgsqlCommand(sql, db)
+                command.ExecuteNonQuery() |> ignore
+                return Ok $"Successfully executed SQL: \n{sql}"
+            with ex ->
+                return Error $"Error executing SQL: \n{ex.Message}"
+        }
 
-    let executeSQLWithLogging (db: NpgsqlConnection) (sql: string) (before: string) (after: string) (error: string) =
-        try
-            printfn "%s" before
-            executeSQL db sql
-            printfn "%s" after
-        with ex ->
-            printfn "%s %s" error ex.Message
-
-    let connectToDB (connStr: string) = // side-effect
-        try
-            use conn = new NpgsqlConnection(connStr)
-            conn.Open()
-            conn
-        with ex ->
-            failwithf "Error connecting to database: %s" ex.Message
+    let connectToDB (connStr: string) callback =
+        async {
+            try
+                use db = new NpgsqlConnection(connStr)
+                db.Open()
+                return! callback db
+            with ex ->
+                return Error $"Error connecting to database: \n{ex.Message}"
+        }
 
     let getFileData (filePath: string) =
-        try
-            File.ReadAllText(filePath)
-        with ex ->
-            failwithf "Error reading file: %s" ex.Message
+        async {
+            try
+                let data = File.ReadAllText(filePath)
+                return Ok data
+            with ex ->
+                return Error $"Error reading file: \n{ex.Message}"
+        }
+
+    let executeSQLFromFile (connStr: string) (filePath: string) =
+        asyncResult {
+            let! sql = getFileData filePath
+            return! connectToDB connStr (fun db -> executeSQL db sql)
+        }
 
 // Setup - creates a database and may seed it among other things
 module Setup =
+    type TestDatabaseConfig =
+        { DefaultConnStr: string
+          ConnStr: string
+          DbName: string
+          SchemaPath: string
+          SeedPath: string }
+
     let createDBIfNotExists (connStr: string) (dbName: string) =
-        use db = DBOps.connectToDB connStr // side-effect
+        async {
+            let! dbExistsResult =
+                DBOps.connectToDB connStr (fun db ->
+                    DBOps.querySQL db $"SELECT 1 FROM pg_database WHERE datname = '{dbName}'")
 
-        use reader =
-            (new NpgsqlCommand(sprintf "SELECT 1 FROM pg_database WHERE datname = '%s'" dbName, db))
-                .ExecuteReader()
+            match dbExistsResult with
+            | Ok rows when rows |> List.isEmpty |> not -> return Ok $"Database already exists: \n{dbName}" // DB exists, no need to create
+            | Ok _ -> return! DBOps.connectToDB connStr (fun db -> DBOps.executeSQL db $"CREATE DATABASE {dbName}") // DB doesn't exist, create it
+            | Error err -> return Error $"Error checking for database existence: \n{err}" // The query failed
+        }
 
-        if not reader.HasRows then
-            DBOps.executeSQLWithLogging
-                db
-                (sprintf "CREATE DATABASE \"%s\"" dbName)
-                "Database does not exist. Creating..."
-                "Database created successfully."
-                "Eror could not create Database."
-        else
-            printfn "Database '%s' already exists." dbName
+    let addDBSchema connStr schemaPath = DBOps.executeSQLFromFile connStr schemaPath
+    let seedTestDB connStr seedPath = DBOps.executeSQLFromFile connStr seedPath
 
-        reader.Close()
-
-    let addDBSchema (connStr: string) (schemaPath: string) =
-        use db = DBOps.connectToDB connStr
-        let schema = DBOps.getFileData schemaPath
-
-        try
-            DBOps.executeSQLWithLogging
-                db
-                schema
-                "Adding Schema to Database..."
-                "Added Schema to Database."
-                "Could not add schema to database."
-        finally
-            db.Close()
-
-    let setupDB (connStr: string) (schemaPath: string) (dbName: string) (successString: string) =
-        createDBIfNotExists connStr dbName
-        addDBSchema connStr schemaPath
-        printfn "%s" successString
+    let setupDB
+        ({ DefaultConnStr = defaultConnStr
+           ConnStr = connStr
+           DbName = dbName
+           SchemaPath = schemaPath
+           SeedPath = seedPath }: TestDatabaseConfig)
+        =
+        asyncResult {
+            let! _ = createDBIfNotExists defaultConnStr dbName
+            let! _ = addDBSchema connStr schemaPath
+            let! _ = seedTestDB connStr seedPath
+            return ()
+        }
 
     let createTestDB () =
         setupDB
-            (EnvOps.getEnv "DB_CONNECTION_STRING")
-            (EnvOps.schemaPath ())
-            (EnvOps.getEnv "TEST_DATABASE_NAME")
-            "Database schema created successfully!"
-
-    let seedTestDB dbEnv seedPath (successString: string) =
-        let dbConn = Environment.GetEnvironmentVariable(dbEnv)
-
-        if String.IsNullOrWhiteSpace dbConn then
-            failwith "Missing database connection environment variable."
-
-        let seedScript = System.IO.File.ReadAllText(seedPath)
-
-        let result =
-            use p = new Process()
-            p.StartInfo.FileName <- "psql"
-            p.StartInfo.Arguments <- $"-c \"{seedScript}\" {dbConn}"
-            p.StartInfo.RedirectStandardOutput <- true
-            p.StartInfo.RedirectStandardError <- true
-            p.Start() |> ignore
-            p.WaitForExit()
-            p.StandardOutput.ReadToEnd()
-
-        if not (result.Contains(successString)) then
-            failwith $"Failed to seed test database: {result}"
-
-        printfn "Test database seeded successfully."
+            { DefaultConnStr = EnvOps.getEnv "DEFAULT_DB_CONNECTION_STRING"
+              ConnStr = EnvOps.getEnv "DB_CONNECTION_STRING"
+              DbName = EnvOps.getEnv "TEST_DATABASE_NAME"
+              SchemaPath = EnvOps.schemaPath ()
+              SeedPath = EnvOps.seedPath () }
 
 // Teardown - will destroy the test database
 module Teardown =
